c8462ccea72f1d243fbe96df24362c0061535da6
Bake <bakerst910@gmail.com>
2016-07-21 12:08:59 -0400

    updated form.html and user.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721144534) do
+ActiveRecord::Schema.define(version: 20160721155934) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
@@ -19,14 +19,13 @@ ActiveRecord::Schema.define(version: 20160721144534) do
     t.text     "ingredient_list"
     t.text     "instructions"
     t.integer  "expected_time"
-
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.integer  "user_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,6 +43,10 @@ ActiveRecord::Schema.define(version: 20160721144534) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.string   "username"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
