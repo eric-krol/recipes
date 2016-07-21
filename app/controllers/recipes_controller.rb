697560c166674093171d684e8635932207ca337f
Juile Harrow <julie.ann.harrow@gmail.com>
2016-07-21 11:22:38 -0400

    made the form partial and controller actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,8 +6,38 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def show
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+      if @recipe.save
+        redirect_to @recipe
+      else
+        redirect_to :new
+      end
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      redirect_to :edit
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title,:description,:ingredient_list,:instructions,:expected_time,:avatar, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'recipes#index'
-  
+  resources :recipes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
