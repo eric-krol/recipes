c9eae99c544b6fc41630bd5fe84562361018dd10
fletcher <fletcherupchurch@gmail.com>
2016-07-27 07:38:51 -0400

    changed background image

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
+  post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users
   root 'recipes#index'
-  resources :recipes
+  resources :recipes do
+    resources :comments, only: :create
+  end
+
+  get 'my_recipes' => 'recipes#my_recipes'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
